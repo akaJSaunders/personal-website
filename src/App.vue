f1c2c7861999aea65e77df0fb5cff21879872e86
akajsaunders <me@jsaunders.dev>
2024-02-23 18:23:05 +0000

    change name to juliano

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,10 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img alt="logo" class="logo" src="@/assets/profile.jpg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Juliano Saunders" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
